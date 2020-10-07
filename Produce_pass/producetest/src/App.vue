98d74e11132efa76c78d479072d0c8afaea04acd
iulianerc <iulianerc>
2020-10-08 00:57:29 +0300

    Shop task

---
+++ Produce_pass/producetest/src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import Products from "@/components/Products";
+
+export default {
+  name: 'App',
+  components: {
+    Products
+  }
+}
+</script>
+
+<template>
+   <div id="app">
+
+     <Products ></Products>
+
+   </div>
+ </template>
---
+++ Produce_pass/producetest/src/components/Products.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>Shops products</h2>
+    <ul>
+      <li v-for="product in products" :key="product.id">{{product}} <button @click.once="addproduct(product)">+</button></li>
+    </ul>
+    <h2>Your Shopping cart with products</h2>
+    <ul>
+      <li v-for="shops in shopcart" :key="shops.id">{{shops}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Produces",
+  data(){
+    return {
+      products: ['smartphone', 'laptop', 'pizza', 'gamepad'],
+      shopcart: ['nons']
+    }
+  },
+  methods:{
+    addproduct($value){
+      this.shopcart.push($value);
+    }
+  }
+}
+</script>
