2f1dcbdcb0b4baa6bc4c951a9d472a9e56dbbe82
iulianerc <iulianerc>
2020-10-08 01:00:52 +0300

    Shop task

--- Produce_pass/producetest/src/components/Products.vue
+++ Produce_pass/producetest/src/components/Products.vue
@@ -1,16 +1,3 @@
-<template>
-  <div>
-    <h2>Shops products</h2>
-    <ul>
-      <li v-for="product in products" :key="product.id">{{product}} <button @click.once="addproduct(product)">+</button></li>
-    </ul>
-    <h2>Your Shopping cart with products</h2>
-    <ul>
-      <li v-for="shops in shopcart" :key="shops.id">{{shops}}</li>
-    </ul>
-  </div>
-</template>
-
 <script>
 export default {
 name: "Produces",
@@ -27,3 +14,16 @@ name: "Produces",
   }
 }
 </script>
+
+<template>
+  <div>
+    <h2>Shops products</h2>
+    <ul>
+      <li v-for="product in products" :key="product.id">{{product}} <button @click.once="addproduct(product)">+</button></li>
+    </ul>
+    <h2>Your Shopping cart with products</h2>
+    <ul>
+      <li v-for="shops in shopcart" :key="shops.id">{{shops}}</li>
+    </ul>
+  </div>
+</template>
